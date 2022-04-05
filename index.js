c8edd75b772d32629aeebb6af931ec68939da0d6
Joseph O. Odhiambo <jodhiambo1@usiu.ac.ke>
2022-04-05 12:38:05 +0300

    calulate btn id added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+
 const port = process.env.PORT || 3000;
+console.log(`server listening on port ${port}`);
+
 const bodyParser = require('body-parser');
 console.log(`server listening on port ${port}`);
 const { acceptsLanguages } = require('express/lib/request');
